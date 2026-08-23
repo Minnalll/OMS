9a1675973ef0a01fbb35c0b9cec930c9b2f5418a
Minnalll <udayakumar.1990@outlook.com>
2026-08-23 12:37:45 +0530

    Part 6.2 – Spring Security Configuration Current Progress ✔ User Entity ✔ RefreshToken Entity ✔ UserRepository ✔ RefreshTokenRepository ✔ DTOs ✔ PasswordEncoder ✔ CustomUserDetails ✔ CustomUserDetailsService ✔ Security Configuration

---
+++ AuthService/src/main/java/com/oms/auth/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.oms.auth.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.oms.auth.model.User;
+import com.oms.auth.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public CustomUserDetailsService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = repository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return new CustomUserDetails(user);
+
+    }
+
+}
