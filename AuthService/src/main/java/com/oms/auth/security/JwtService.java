8fe16270192e38331752dd9394aa80c709f069e3
Minnalll <udayakumar.1990@outlook.com>
2026-08-23 16:58:50 +0530

    Part 6.4.3 – Login Implementation ✅ User Registration ✅ BCrypt password encryption ✅ User authentication using AuthenticationManager ✅ JWT Access Token generation ✅ JWT Refresh Token generation ✅ Refresh token persistence ✅ Login API

---
+++ AuthService/src/main/java/com/oms/auth/security/JwtService.java
@@ -0,0 +1,175 @@
+package com.oms.auth.security;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import java.util.HashMap;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import com.oms.auth.properties.JwtProperties;
+
+@Service
+public class JwtService {
+
+    private final Key secretKey;
+    private final JwtProperties jwtProperties;
+
+    public JwtService(Key secretKey,
+                      JwtProperties jwtProperties) {
+
+        this.secretKey = secretKey;
+        this.jwtProperties = jwtProperties;
+
+    }
+
+    public String generateAccessToken(String username) {
+
+        return Jwts.builder()
+
+                .subject(username)
+
+                .issuedAt(new Date())
+
+                .expiration(new Date(
+
+                        System.currentTimeMillis()
+
+                                + jwtProperties.getAccessTokenExpiration()
+
+                ))
+
+                .signWith(secretKey, SignatureAlgorithm.HS512)
+
+                .compact();
+
+    }
+
+    public String generateAccessToken(
+            String username,
+            Map<String, Object> claims) {
+
+        return Jwts.builder()
+
+                .claims(claims)
+
+                .subject(username)
+
+                .issuedAt(new Date())
+
+                .expiration(new Date(
+
+                        System.currentTimeMillis()
+
+                                + jwtProperties.getAccessTokenExpiration()
+
+                ))
+
+                .signWith(secretKey, SignatureAlgorithm.HS512)
+
+                .compact();
+
+    }
+
+    public String generateRefreshToken(String username) {
+
+        return Jwts.builder()
+
+                .subject(username)
+
+                .issuedAt(new Date())
+
+                .expiration(new Date(
+
+                        System.currentTimeMillis()
+
+                                + jwtProperties.getRefreshTokenExpiration()
+
+                ))
+
+                .signWith(secretKey, SignatureAlgorithm.HS512)
+
+                .compact();
+
+    }
+
+    public String extractUsername(String token) {
+
+        return extractClaim(
+
+                token,
+
+                Claims::getSubject
+
+        );
+
+    }
+
+    public <T> T extractClaim(
+
+            String token,
+
+            Function<Claims, T> resolver) {
+
+        Claims claims = extractAllClaims(token);
+
+        return resolver.apply(claims);
+
+    }
+
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+
+                .verifyWith((javax.crypto.SecretKey) secretKey)
+
+                .build()
+
+                .parseSignedClaims(token)
+
+                .getPayload();
+
+    }
+
+    public Date extractExpiration(String token) {
+
+        return extractClaim(
+
+                token,
+
+                Claims::getExpiration
+
+        );
+
+    }
+
+    public boolean isTokenExpired(String token) {
+
+        return extractExpiration(token)
+
+                .before(new Date());
+
+    }
+
+    public boolean validateToken(
+
+            String token,
+
+            String username) {
+
+        String extractedUser =
+
+                extractUsername(token);
+
+        return extractedUser.equals(username)
+
+                && !isTokenExpired(token);
+
+    }
+
+
+
+}
